1dc8d076bc54473d29c6bdf3e91103f6149cce98
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-07 20:19:38 +0100

    Initial commit

---
+++ TestNinja/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNinja.TestNinja.UnitTests.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
---
+++ UnitTest1/ReservationTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_Scenario_ReturnTrue()
+        {
+            // arrange
+
+            // act
+
+            //assert
+        }
+    }
+}
